2c98387e644ca947e30239f9ac767b3642ad47ed
MohamedElmdary <engm5081@gmail.com>
2021-08-21 12:48:24 +0200

    Added docs for controller

--- nest/core/controller.py
+++ nest/core/controller.py
@@ -6,6 +6,50 @@ __assert_list = assert_list('`controller` decorator')
 
 
 def controller(prefix: str = None, injects: list = None):
+    """
+    Marks a class as a controller.
+
+    Adding a meta attribute with controller configs which allows to
+    initialize class correctly with the required options also accepts
+    a prefix which will be used later with routes.
+
+    Parameters
+    ----------
+    prefix : str
+        A string that will be appended before each route's uri.
+    injects : list
+        A list of required services that should be passed into the
+        controller should it can perform correctly.
+
+    Returns
+    -------
+    Any
+        Returns a function that takes a class and returns it after registering meta on it.
+
+    Examples
+    --------
+    >>> @controller({
+    >>>     prefix='/user',
+    >>>     injects=[
+    >>>         UserService
+    >>>     ]
+    >>> })
+    >>> class UserController:
+    >>> 
+    >>>     def __init__(self, userService: UserService):
+    >>>         self.userService = userService
+
+    See Also
+    --------
+    nest_error : 
+        Marks a class as an error_handler.
+    nest_module :
+        Marks a class as an module (Aka. Bottle app).
+    injectable :
+        Marks a class as a service that can be injected later on controllers
+        or in other services.
+
+    """
     __assert_str(prefix, 'prefix')
     __assert_list(injects, 'injects')
 
