3cd1ed5b404705b210765fd4d45c4951d82835bc
MohamedElmdary <engm5081@gmail.com>
2021-08-26 00:52:16 +0200

    Refactor core code

--- nest/scripts/__init__.py
+++ nest/scripts/__init__.py
@@ -1,10 +1,3 @@
 from .types import Types
 from .app_context import AppContext
-from .create_error_callback import create_error_callback
 from .assert_type import assert_str, assert_list
-from .create_route_callback import create_route_callback
-from .register_app_routes import register_app_routes
-from .create_error_handlers import create_error_handlers
-from .install_plugins import install_plugins
-from .merge_apps import merge_apps
-from .compile import compile
